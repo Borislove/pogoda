53760ae5af49f2dc9a3aec84d3f283866695b501
Boris <abirme@yandex.ru>
2020-04-07 11:23:47 +0300

    Initial commit

---
+++ src/main/java/Parser.java
@@ -0,0 +1,42 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    private static Document getPage() throws IOException {
+        String url = "https://pogoda.spb.ru/";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        return page;
+    }
+
+    private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
+
+    private static String getDateFromString(String stringDate) throws Exception {
+        Matcher matcher = pattern.matcher(stringDate);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        throw new Exception("Can't extract date from string!");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Document page = getPage();
+        Element tableWth = page.select("table[class=wt]").first();
+        System.out.println(tableWth);
+        Elements names = tableWth.select("tr[class=wth]");
+        Elements values = tableWth.select("tr[valign=top]");
+
+        for (Element name : names) {
+            String dateString = name.select("th[id=dt]").text();
+            String date = getDateFromString(dateString);
+            System.out.println(date + "   Явления   Температура    Давление    Влажность   Ветер");
+        }
+    }
+}
